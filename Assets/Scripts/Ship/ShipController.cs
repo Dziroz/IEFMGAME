57d740eaa150bd274084b8f1872b7a103f83925f
Dziroz <abcdanil@bk.ru>
2024-03-17 19:44:16 +0500

    Новый корабль и его передвижение

--- Assets/Scripts/Ship/ShipController.cs
+++ Assets/Scripts/Ship/ShipController.cs
@@ -5,30 +5,72 @@ using UnityEngine;
 public class ShipController : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] private float maxSpeed;
     [SerializeField] private float speed;
+    [SerializeField] private float boost;
+    [SerializeField] private float timer;
+    [SerializeField] private float timerSpeed;
     [SerializeField] private float angleSpeed;
     void Start()
     {
+        maxSpeed = speed;
+        speed = 0;
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        timer += Time.deltaTime;
     }
     private void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.W) && timer >= timerSpeed)
+        {
+            if(speed<= maxSpeed)
+            {
+                timer = 0;
+                speed += boost;
+            }
+        }
+        else if(Input.GetKey(KeyCode.S) && timer >= timerSpeed)       
+        {
+            if (speed >= -maxSpeed)
+            {
+                timer = 0;
+                speed -= boost;
+            }
+        }
+        else
+        {
+            if (timer >= timerSpeed)
+            {
+                if (speed != 0)
+                {
+                    if (speed > 0)
+                    {
+                        speed -= boost;
+                        timer = 0;
+                    }
+                    else
+                    {
+                        speed += boost;
+                        timer = 0;
+                    }
+                }
+
+            }
+        }
         Move();
     }
     private void Move()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddRelativeForce(speed, 0, 0);
+            //rb.AddRelativeForce(speed, 0, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddRelativeForce(-speed, 0, 0);
+            //rb.AddRelativeForce(-speed, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
@@ -40,5 +82,6 @@ public class ShipController : MonoBehaviour
             rb.AddRelativeTorque(0, 0, -angleSpeed);
             Debug.Log("left");
         }
+        rb.AddRelativeForce(speed, 0, 0);
     }
 }
